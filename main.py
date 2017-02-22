20a876b483821d8dd8dce28276c33fe3747db6ea
nazar-romaniv <nazarromaniv999@gmail.com>
2017-02-22 02:37:18 +0200

    Fix the issue with the gratuitous code in Apartment

--- main.py
+++ main.py
@@ -74,24 +74,6 @@ class Apartment(Property):
         print("APARTMENT DETAILS")
         print("laundry: {}".format(self.laundry))
         print("has balcony:".format(self.balcony))
-        parent_init = Property.prompt_init()
-        laundry = ''
-        while laundry.lower() not in \
-                Apartment.valid_laundries:
-            laundry = input("What laundry facilities does "
-                            "the property have? ({})".format(
-                            ", ".join(Apartment.valid_laundries)))
-        balcony = ''
-        while balcony.lower() not in Apartment.valid_balconies:
-            balcony = input(
-                            "Does the property have a balcony? "
-                            "({})".format(
-                            ", ".join(Apartment.valid_balconies)))
-        parent_init.update({
-                            "laundry": laundry,
-                            "balcony": balcony
-        })
-        return parent_init
 
     @staticmethod
     def prompt_init():
